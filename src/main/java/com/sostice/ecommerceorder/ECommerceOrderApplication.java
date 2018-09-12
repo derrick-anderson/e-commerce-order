dde3c2620b157328bb05a410ecd6533b7b441849
Derrick Anderson <deanderson@solstice.com>
2018-09-12 13:41:31 -0500

    Inital commit

---
+++ src/main/java/com/sostice/ecommerceorder/ECommerceOrderApplication.java
@@ -0,0 +1,12 @@
+package com.sostice.ecommerceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceOrderApplication.class, args);
+    }
+}
